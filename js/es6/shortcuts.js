31f89df619412db84c15ecb3aaebb469643b1931
Robin Curts <Robin Curts>
2017-03-25 09:57:41 -0400

    removing unneeded comments

--- js/es6/shortcuts.js
+++ js/es6/shortcuts.js
@@ -1,6 +1,4 @@
 // [--Shortcut for document.getElementById()----------------------------------]
 // Usage: $('id_of_element')
-
 const $ => s => document.getElementById(s);
-// [--E-----------------------------------------------------------------------]
 
